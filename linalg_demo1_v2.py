145cd46296c0eb37c6f07abfbccf28bac2be66e5
Fumito Nakamura <ffumituchi@gmail.com>
2019-10-17 22:02:30 +0900

    update for linalg
    update for linalg

--- linalg_demo1_v2.py
+++ linalg_demo1_v2.py
@@ -40,6 +40,15 @@ from ipywidgets import interact
 # $$
 # ここでは、画像の拡大、せん断、回転を見てみる
 
+X = np.array([[0,0,0,0,0,0,0],[0,1,1,1,1,1,0],[0,0,0,1,0,0,0],[0,0,0,1,0,0,0],[0,0,0,1,0,0,0],[0,0,0,1,0,0,0],[0,0,0,0,0,0,0]])
+(pos_x, pos_y) = np.where(X == 1)
+
+# ## 対象となる画像:
+
+plt.imshow(X)
+plt.show()
+
+
 # ## 画像の拡大
 # + 拡大行列$P(\alpha, \beta)$は
 # $$
@@ -70,10 +79,6 @@ from ipywidgets import interact
 # $$
 # + ** 画像の行き先をドット上にしてするため四捨五入しているので割と粗いです **
 
-X = np.array([[0,0,0,0,0,0,0],[0,1,1,1,1,1,0],[0,0,0,1,0,0,0],[0,0,0,1,0,0,0],[0,0,0,1,0,0,0],[0,0,0,1,0,0,0],[0,0,0,0,0,0,0]])
-(pos_x, pos_y) = np.where(X == 1)
-
-
 def im_extend(alpha, beta, offset_x, offset_y):
     """
     画像を拡大する関数
@@ -97,24 +102,38 @@ def im_extend(alpha, beta, offset_x, offset_y):
     plt.show()
 
 
-interact(im_extend, alpha = (-2, 2, 0.1), beta=(-2, 2, 0.1), offset_x = (0, 7, 1), offset_y=(0, 7, 1))
+interact(im_extend, alpha = (0, 2, 0.1), beta=(0, 2, 0.1), offset_x = (0, 7, 1), offset_y=(0, 7, 1))
 
+
+# ## 画像の拡大
+# + 拡大行列$P(\alpha, \beta)$は
+# $$
+# Q(\gamma) = \left[\begin{array}{cc}
+#     1 & \gamma \\
+#     0 & 1 \\
+# \end{array}\right]
+# $$
+# 特に$(a,b)$周りで拡大したとき、変換後の位置$(x', y')$は
+# $$
+# \left[\begin{array}{c}
+#     x' \\
+#     y' \\
+# \end{array}\right] = 
+# \left[\begin{array}{cc}
+#     1 & \gamma \\
+#     0 & 1 \\
+# \end{array}\right]
+# \left[\begin{array}{c}
+#     x - a \\
+#     y - b \\
+# \end{array}\right]
 # +
-# np.random.seed(20191012)
-# X = np.random.normal(size = (100, 100))
-
-# +
-# X = np.array([[1,1,1],[0,0,1],[0,0,1]])
-# -
-
-theta = np.pi / 5
-R = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta), np.cos(theta)]])
-offset_x = 3
-offset_y = 3
-
-offset_x = 3
-offset_y = 3
-
+# \left[\begin{array}{c}
+#     a \\
+#     b \\
+# \end{array}\right]
+# $$
+# + ** 画像の行き先をドット上にしてするため四捨五入しているので割と粗いです **
 
 def im_shear(gamma, offset_x, offset_y):
     """
@@ -201,27 +220,49 @@ n = 200
 data_seed = 20191012
 np.random.seed(data_seed)
 
-(a,b) = (-2, -0.5)
+(a,b) = (-3, 5)
 
-# ## $x ~ uniform(-10, 10)$, $y=ax+b + noise$に従ってデータを発生させる
+# ## $x \sim U(-10, 10)$, $y=ax+b + noise$に従ってデータを発生させる
 
-domain_X = (-10, 10)
+domain_X = (-5, 5)
 X = np.random.uniform(low = domain_X[0], high = domain_X[1], size = n)
 Y = a*X + b + np.random.normal(scale = 4, size = n)
 original_data = np.array([X,Y]).T
+
+test_X = np.linspace(start = domain_X[0], stop = domain_X[1], num = 100)
+test_Y = a*test_X + b
 
 plt.scatter(X,Y)
 plt.show()
 
 # ## 情報は$ax + b$に集約されているので、この(x,y)を線分上に射影したい
 
+chose_ind = np.random.permutation(n)
+chose_num = 5
+plt.plot(test_X, test_Y)
+for i in range(chose_num):
+    d = np.array([1, a])
+    p0 = np.array([0, b])
+    p = (X[chose_ind[i]], Y[chose_ind[i]])
+    length = (-d@(p0-p))/(d@d)
+    OS = p0 + length*d
+    data_X = np.array([p[0], OS[0]])
+    data_Y = np.array([p[1], OS[1]])
+    plt.scatter(data_X, data_Y)
+plt.show()
+
+# ## 求め方
+# + 以下の関数$L(w)$の最大化を行えばよい
+# $$
+# L(w) = \sum_{i=1}^n (x_i^T w)^2
+# $$
+# 感覚的には、x_iのばらつきが大きい方向を見つけて、そのばらつきを除去するようなことをしている。  
+# $w^T w = 1$の制約の下でこれを解くと、$X^T X$に対する教科書の固有値問題を解くのと同じになる:
+
 norm_mean = original_data.mean(axis = 0)
 norm_data = original_data - norm_mean
 
 cov_mat = norm_data.T @ norm_data
-
-plt.scatter(norm_data[:,0], norm_data[:,1])
-plt.show()
 
 
 def is_pos_def(A:np.ndarray):
@@ -239,19 +280,18 @@ if is_pos_def(cov_mat):
     [eig_val, eig_vec] = np.linalg.eigh(cov_mat)
     compressed_proj = eig_vec[:,1]
 
-test_X = np.linspace(start = domain_X[0], stop = domain_X[1], num = 100)
-test_Y = a*test_X + b
+proj_data = (norm_data @ compressed_proj).reshape(n,1) @ compressed_proj.reshape(1,2) + norm_mean
 
-proj_data = (norm_data @ compressed_proj).reshape(n,1) @ compressed_proj.reshape(1,2)
+# +
+# focus_ind = 9
+# plt.plot(test_X, test_Y)
+# plt.scatter(original_data[focus_ind,0], original_data[focus_ind,1])
+# plt.scatter(proj_data[focus_ind,0], proj_data[focus_ind,1])
+# plt.show()
+# -
 
-focus_ind = 9
 plt.plot(test_X, test_Y)
-plt.scatter(original_data[focus_ind,0], original_data[focus_ind,1])
-plt.scatter(proj_data[focus_ind,0], proj_data[focus_ind,1])
-plt.show()
-
-plt.plot(test_X, test_Y)
-plt.scatter(original_data[:,0], original_data[:,1])
+# plt.scatter(original_data[:,0], original_data[:,1])
 plt.scatter(proj_data[:,0], proj_data[:,1])
 plt.show()
 
@@ -318,15 +358,17 @@ plt.show()
 # + $A + B$の固有値は$\lambda_A + \lambda_B$で($\lambda_A$は$A$の固有値, $\lambda_B$は$B$の固有値), $\beta I$の固有値は$\beta$, $X^T X$の固有値は実は$\geq 0$ (証明は今後出てくるかもしれない)なので、$X^T X + \beta I$の固有値は$>0$
 #
 
-n = 20
+n = 10
 data_seed = 20191012
 np.random.seed(data_seed)
 
 domain_X = (-5, 5)
 train_X = np.ones((n,2))
 train_X[:,1] = np.random.uniform(low = domain_X[0], high = domain_X[1], size = n)
-true_w = np.array([-2, 3])
-train_Y = train_X @ true_w + np.random.normal(scale = 2, size = n)
+true_w = np.array([2, 3])
+train_Y = train_X @ true_w + np.random.normal(scale = 4, size = n)
+
+np.linalg.eigh(train_X.T @ train_X)
 
 beta = 2
 
@@ -350,17 +392,18 @@ test_est_ridge_Y = test_X @ est_ridge_w
 
 est_mle_w, est_ridge_w
 
-np.linalg.solve(train_X.T @ train_X + beta * np.eye(2), train_X.T)
+# +
+# np.linalg.solve(train_X.T @ train_X + beta * np.eye(2), train_X.T)
 
-np.linalg.solve(train_X.T @ train_X, train_X.T)
+# +
+# np.linalg.solve(train_X.T @ train_X, train_X.T)
+# -
 
 plt.plot(test_X[:,1], test_true_Y, label = "true")
 plt.plot(test_X[:,1], test_est_mle_Y, label ="mle")
 plt.plot(test_X[:,1], test_est_ridge_Y, label = "ridge")
 plt.legend()
 plt.show()
-
-
 
 # + mleが引っ張られる理由はラグランジュの未定乗数法から出るが、線形代数学の範囲外なので割愛
 # + 
