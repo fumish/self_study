237fbdbbc642bc6b3eab00da9c74e88a6c8b2979
Fumito Nakamura <ffumituchi@gmail.com>
2020-11-07 22:58:20 +0900

    Numerical expriments to show a unique solution of the regression.
    Pushed notebook shows uniqueness of solution for the vb linear regression with laplace prior.

---
+++ laplace_prior/.ipynb_checkpoints/laplace_prior_unique_solution-checkpoint.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Discussion about a unique solution of vb linear regression with a laplace prior.
+# In the context of VB linear regression, penalty term of the laplace prior is a little bit different from l1-normalization
+# + 
